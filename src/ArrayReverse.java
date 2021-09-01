105165ecb156064008501b62fa8691ef8e005886
Rinku Gopali <50997971+rgopali@users.noreply.github.com>
2021-08-31 20:45:01 -0500

    added stream and stringbuilder methods

--- src/ArrayReverse.java
+++ src/ArrayReverse.java
@@ -1,17 +1,47 @@
 import java.util.Scanner;
 
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 //Reverse array - brute force method - loops - 0(n)
+//public class ArrayReverse{
+//    public static void main(String[] args) {
+//        String str , reverseStr = "";
+//        Scanner in = new Scanner(System.in);
+//        System.out.println("Enter String: ");
+//        str = in.nextLine();
+//
+//        for(int i=str.length()-1; i >=0; i--){
+//            reverseStr += str.charAt(i);
+//        }
+//        System.out.println("Reversed string: " + reverseStr);
+//    }
+//}
+
+// Reverse array using String Builder
+//public class ArrayReverse{
+//    public static void main(String[] args) {
+//        System.out.println(ArrayReverse.reverse("Hi There"));
+//
+//    }
+//
+//    public static String reverse(String str){
+//        return new StringBuilder(str).reverse().toString();
+//    }
+//}
+
+// Reverse array using Stream
 public class ArrayReverse{
     public static void main(String[] args) {
-        String str , reverseStr = "";
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter String: ");
-        str = in.nextLine();
+        System.out.println(ArrayReverse.reverse("Hi There Hello"));
 
-        for(int i=str.length()-1; i >=0; i--){
-            reverseStr += str.charAt(i);
-        }
-        System.out.println("Reversed string: " + reverseStr);
+    }
+
+    public static String reverse(String str){
+        return Stream.of(str)
+                .map(word->new StringBuilder(word).reverse())
+                .collect(Collectors.joining(" "));
     }
 }
+
+
